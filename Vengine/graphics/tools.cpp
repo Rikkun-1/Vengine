1ceceba210046df19659db8d2c15f60643cc7cb7
Rikkun-1 <torokhov.nickita@yandex.ru>
2020-12-16 05:19:43 +0700

    Небольшие изменения

--- Vengine/graphics/tools.cpp
+++ Vengine/graphics/tools.cpp
@@ -4,7 +4,7 @@
 #include "libraries/tiny_obj_loader.h"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include "libraries/stb_image.h"
 
 ///////////////////////// STATIC BEG /////////////////////////////
 
